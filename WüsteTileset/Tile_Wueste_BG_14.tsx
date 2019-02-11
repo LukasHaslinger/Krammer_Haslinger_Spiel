a337da47deba91930411a9540ffca86bfcc0ca29
Haslinger Lukas <lukas.haslinger14@gmail.com>
2019-02-11 08:38:56 +0100

    Maps,Figuren

---
+++ WüsteTileset/Tile_Wueste_BG_14.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="BG" tilewidth="14" tileheight="14" tilecount="540" columns="27">
+ <image source="svg/BG.svg" trans="000000" width="383" height="287"/>
+</tileset>
